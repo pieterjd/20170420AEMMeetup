f7586955c91025cde535a2a19f1b01a93a0249a2
Pieter-Jan Drouillon <Pieter-Jan.Drouillon@telenetgroup.be>
2017-04-10 10:03:47 +0200

    added dialog and updated rendering and logic.

--- content/src/main/content/jcr_root/apps/meetup20170420/components/trivia/trivia.js
+++ content/src/main/content/jcr_root/apps/meetup20170420/components/trivia/trivia.js
@@ -1,7 +1,12 @@
 //this function return a random fact
-use(function () {
+use(["/libs/wcm/foundation/components/utils/ResourceUtils.js" ],function (ResourceUtils) {
     // you can reference the parameters via the this keyword.
+    var result={};
     var facts = properties.facts;
     var randomIndex = Math.floor(Math.random() * facts.length);
-    return facts[randomIndex];
+    result.fact = facts[randomIndex];
+    result.nrFacts=facts.length;
+    result.imageUploaded = resource.getChild('file') != null;
+    result.showImage = Math.floor(Math.random() * 100) <= properties.randomCutoffValue;
+    return result;
 });
